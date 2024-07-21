fea49f4822961a55850861d3ea8411095ae3fd7c
Reniel George Mababa <mabsrencode@gmail.com>
2024-07-21 13:25:57 +0800

    build

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 const express = require("express");
+const cron = require("node-cron");
+const http = require("http");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const path = require("path");
@@ -13,7 +15,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
@@ -23,28 +25,39 @@ app.use(cookieParser());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
-// app.use(express.static(path.join(__dirname, "../client/build")));
-
-// app.get("/*", function (req, res) {
-//   res.sendFile(path.join(__dirname, "../client/build", "index.html"));
-// });
-
-const dbUrl = process.env.DB_URL;
-const PORT = process.env.PORT || 4000;
-
-app.use((req, res, next) => {
-  console.log(req.path, req.body);
-  next();
-});
 app.use("/auth", authRoutes);
 app.use("/contact", contactRoutes);
 app.use("/job", jobOffers);
 app.use("/admin", adminRoutes);
+app.use((req, res, next) => {
+  console.log(req.path, req.body);
+  next();
+});
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+const dbUrl = process.env.DB_URL;
+const PORT = process.env.PORT || 4000;
+
+const pingServer = () => {
+  http
+    .get("localhost", (res) => {
+      console.log("Pinged server, status code:", res.statusCode);
+    })
+    .on("error", (err) => {
+      console.error("Error pinging server:", err.message);
+    });
+};
 connectDB()
   .then(() => {
     app.listen(PORT, () => {
       console.log(`Starting server on port ${PORT}`);
     });
+    cron.schedule("*/5 * * * *", pingServer);
   })
   .catch((error) => {
     console.error("Error connecting to database:", error);
