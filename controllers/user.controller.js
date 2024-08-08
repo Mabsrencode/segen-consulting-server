d2a774489c2727b16da7655754315d2cf2f8fd44
Reniel George Mababa <mabsrencode@gmail.com>
2024-08-09 05:44:05 +0800

    creating router and controller for deleting admin account and changing user role

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -5,4 +5,38 @@ const getAllUsers = async (req, res) => {
   res.status(200).send(data);
 };
 
-module.exports = { getAllUsers };
+const deleteUser = async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.json({ message: "User account deleted" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+const changeRoleUser = async (req, res) => {
+  try {
+    const { id, newRole } = req.body;
+
+    // Validate newRole
+    if (!["admin", "super_admin"].includes(newRole)) {
+      return res.status(400).json({ message: "Invalid role" });
+    }
+
+    const user = await User.findByIdAndUpdate(
+      id,
+      { role: newRole },
+      { new: true }
+    );
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json({ message: "User role updated successfully", user });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { getAllUsers, deleteUser, changeRoleUser };
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { getAllUsers } = require("../controllers/user.controller.js");
+const {
+  getAllUsers,
+  deleteUser,
+  changeRoleUser,
+} = require("../controllers/user.controller.js");
 router.get("/all-users", getAllUsers);
-
+router.delete("/delete-account/:id", deleteUser);
+router.put("/change-role", changeRoleUser);
 module.exports = router;
